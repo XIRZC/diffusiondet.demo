5b32ab8fd0b021d92d79fbf16bd97bbe45fa867a
mrxir <zicongxie@foxmail.com>
2022-11-24 15:57:22 +0800

    [INIT COMMIT FROM DIFFUSIONDET] diffusiondet docker build, frontend react app and backend django api left

---
+++ README.md
@@ -0,0 +1,5 @@
+# Demonstration App for `DiffusionDet` with Denoising Process Visualization
+
+This is just a simple demo application for diffusiondet by react and django.
+
+Coming Soon...
---
+++ diffusiondet/infer_once.sh
@@ -0,0 +1,5 @@
+python demo.py \
+--config-file configs/diffdet.coco.swinbase.yaml \
+--input ../pics/COCO_val2014_000000581887.jpg \
+--output infer_once_results \
+--opts MODEL.WEIGHTS models/diffdet_coco_swinbase.pth
