a9cfbfbf9e9e253bd7774989974e5f86548adc05
mrxir <zicongxie@foxmail.com>
2023-02-18 19:37:38 +0800

    [UPD] docker scripts

--- docker_executable.sh
+++ docker_executable.sh
@@ -2,6 +2,6 @@ sudo docker run -it --rm \
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 # -v ${PWD}/../pics/:/work/pics \
-registry.cn-hangzhou.aliyuncs.com/mrxir/diffusiondet.demo:model-executable-v0.1 \
+registry.cn-hangzhou.aliyuncs.com/mrxir/diffusiondet.demo:model-executable \
 --input /work/examples/*
 #--input /work/examples/COCO01.jpg
--- docker_executable_gradio.sh
+++ docker_executable_gradio.sh
@@ -1,5 +1,5 @@
-sudo docker run -it --rm \
+sudo docker run -it --rm \AA
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 # -v ${PWD}/../pics/:/work/pics \
-registry.cn-hangzhou.aliyuncs.com/mrxir/diffusiondet.demo:model-executable-v1.0 \
+registry.cn-hangzhou.aliyuncs.com/mrxir/diffusiondet.demo:model-executable-v2.0 \
